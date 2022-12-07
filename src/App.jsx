2a03ce5386d0af100c55bda31fdca992f698cc42
Tnluser <tnluser@Byjus-MBP.local>
2022-12-07 14:29:10 +0530

    create base template

---
+++ src/App.jsx
@@ -0,0 +1,178 @@
+import { useState, useRef, useEffect } from 'react'
+import styles from './app.module.css'
+import evenPage from './assets/even.jpeg';
+import oddPage from './assets/odd.jpeg';
+import { useReactToPrint } from 'react-to-print';
+
+// main template
+import mainInfoSection from './assets/main_info_section.png';
+import mainSnoSection from './assets/main_sno_section.png';
+import mainFooterSection from './assets/main_footer_section.png';
+
+// even template
+import evenInfoSection from './assets/even_info_section.png';
+import evenSnoSection from './assets/even_sno_section.png';
+import evenFooterSection from './assets/even_footer_section.png';
+
+// odd template
+import oddInfoSection from './assets/odd_info_section.png';
+import oddSnoSection from './assets/odd_sno_section.png';
+import oddFooterSection from './assets/odd_footer_section.png';
+
+const IMAGE_POSITIONS = {
+  TOP: 'top',
+  MIDDLE: 'middle',
+  FOOTER: 'footer'
+}
+
+function App() {
+  const mainRef = useRef()
+  const [pages, setPages] = useState([
+    {
+      id: 1,
+      ref: useRef(),
+      value: "1"
+    },
+    {
+      id: 2,
+      ref: useRef(),
+      value: "2"
+    },
+    {
+      id: 3,
+      ref: useRef(),
+      value: "3"
+    },
+    {
+      id: 4,
+      ref: useRef(),
+      value: "4"
+    },
+    {
+      id: 5,
+      ref: useRef(),
+      value: "5"
+    },
+    {
+      id: 6,
+      ref: useRef(),
+      value: "6"
+    }
+  ])
+  
+  const handleChange = (e, index) => {
+    const {current = {}} = pages[index].ref
+    const {clientHeight, scrollHeight} = current
+    const isOverflow = scrollHeight > clientHeight;
+    console.log({isOverflow, current, e})
+    setPages(pages.map((p,i) => {
+      if(i === index) {
+        p.value = e.target.value;
+      }
+      return p;
+    }))
+    if(isOverflow) {
+      pages[index+1].ref.current.focus()
+    }
+  }
+
+  const handlePrint = useReactToPrint({
+    content: () => mainRef.current,
+  });
+  
+  return (
+    <div ref={mainRef} className={styles.main}>
+      {
+        pages.map((item, index) => {
+          return (
+            <div 
+              key={item.id} 
+              className={styles.page}
+            >
+              {/* info and top section */}
+              <div className={styles.topSection} style={{ height: index % 2 == 1 ? "10%" : "20%"}}>
+                <img src={getImage(IMAGE_POSITIONS.TOP, index)} className={styles.infoImgSection} />
+              </div>
+
+              {/* middle section */}
+              <div className={styles.middleSection} style={{ height: index % 2 == 1 ? "80%" : "70%"}}>
+                <div className={styles.snoSection}>
+                  <img src={getImage(IMAGE_POSITIONS.MIDDLE, index)} className={styles.snoImgSection} />
+                </div>
+                <div className={styles.textarea}>
+                  <textarea 
+                    ref={pages[index].ref} 
+                    className={styles.input} 
+                    onChange={(e) => {handleChange(e, index)}}
+                    value={item.value}
+                  />
+                </div>
+              </div>
+              
+              {/* footer section */}
+              <div className={styles.footerSection}>
+                <img src={getImage(IMAGE_POSITIONS.FOOTER, index)} className={styles.footerImgSection} />
+              </div>
+            </div>
+          )
+        })
+      }
+      <button onClick={handlePrint}>Print this out!</button>
+    </div>
+  )
+}
+
+export default App
+
+
+function getImage(position, index) {
+
+  if( index == 0) {
+    switch(position){
+      case IMAGE_POSITIONS.TOP: {
+        return mainInfoSection
+      };
+      case IMAGE_POSITIONS.MIDDLE: {
+        return mainSnoSection
+      };
+      case IMAGE_POSITIONS.FOOTER: {
+        return mainFooterSection
+      };
+      default: {
+        return mainInfoSection
+      }
+    }
+  } else if(index % 2 == 0) {
+    // odd number case
+    switch(position){
+      case IMAGE_POSITIONS.TOP: {
+        return oddInfoSection
+      };
+      case IMAGE_POSITIONS.MIDDLE: {
+        return oddSnoSection
+      };
+      case IMAGE_POSITIONS.FOOTER: {
+        return oddFooterSection
+      };
+      default: {
+        return oddInfoSection
+      }
+    }
+  } else {
+    // even case
+    switch(position){
+      case IMAGE_POSITIONS.TOP: {
+        return evenInfoSection
+      };
+      case IMAGE_POSITIONS.MIDDLE: {
+        return evenSnoSection
+      };
+      case IMAGE_POSITIONS.FOOTER: {
+        return evenFooterSection
+      };
+      default: {
+        return evenInfoSection
+      }
+    }
+  }
+}
